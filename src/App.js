c8217f75f640f41a8341fa8d33e18866e894c7b6
Matheus Icaro <matheusicaro2@hotmail.com>
2018-10-15 20:20:05 -0300

    initializing structure page home.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/config.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+          <div clasName="header-profile" > HEADER PROFILE </div>
+          <div clasName="header-profile"  style={style}> SEARCH BOOKS </div>
+          
+          {/* <div clasName="stand"> */}
+            <div style={style}> Currently Reading </div>
+            <div style={style}> Want to Read </div>
+            <div style={style}> Read </div>
+          {/* </div> */}
       </div>
     );
   }
 }
 
 export default App;
+
+const style = {
+  'border': 'solid',
+  'border-color': 'blue'
+};
