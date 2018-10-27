19f057fdf87aed2088751ed9c24818627c87024a
Matheus Icaro <matheusicaro2@hotmail.com>
2018-10-27 17:35:35 -0300

    applying style in page 'loading', align center.

--- src/components/presentation/utils/Loading.js
+++ src/components/presentation/utils/Loading.js
@@ -2,10 +2,15 @@ import React from 'react';
 import CircularProgress from 'material-ui/CircularProgress';
 
 const Loading = () => (
-  <div>
-    <h1>Carregando o sistema...</h1>
+  <div style={style}>
     <CircularProgress size={60} thickness={7} />
   </div>
 );
 
 export default Loading;
+
+const style = {
+  position: 'absolute',
+  left: '50%',
+  top: '40%',
+}
