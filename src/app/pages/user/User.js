8431dd0849f81baa8b89c05c0d7391768250cf53
Matheus Icaro <matheusicaro2@hotmail.com>
2018-11-12 18:21:45 -0200

    fix bug style in user page.

--- src/app/pages/user/User.js
+++ src/app/pages/user/User.js
@@ -1,26 +1,12 @@
 import React from 'react'
 
-import image from '../../media/images/profile-avatar.png'
-
-const User = () => {
+const User = ({ language }) => {
     return (
         <div style={styles.root}>
-            {/* <form onSubmit={this.handleSubmit} className='create-contact-form'>
-                <img src={image} alt="Avatar" />
-                
-                <div className='create-contact-details'>
-                    <input type='text' name='name' placeholder='Name' />
-                    <input type='text' name='handle' placeholder='Handle' />
-                    <button>Add Contact</button>
-                </div>
-            </form> */}
 
-            <p>Está página está em construção, assim que possivel será implementado funcionalidades como: </p>
-            <ul>
-                <li>Edição do perfil, dados, foto de perfil, entre outros...</li>
-                <li>A estante de cada livro será salvo em banco de dados, provalvemente no Firebase, com isto, cada usuario poderá guardar o seu perfil.</li>
-                <li>Altentição com redes sociais.</li>
-                <li>Entre outras novidades...</li>
+            <p style={styles.p}> {language.intro}</p>
+            <ul style={styles.ul}>
+                {language.topics.map((topic,index) => <li key={index}>{ topic }</li>)}
             </ul>
 
         </div>
@@ -31,9 +17,13 @@ export default User
 
 const styles = {
     root: {
-        position: 'relative',
-        left: '20%',
         fontSize: '20px',
         color: '#ff0000cc'
+    },
+    p: {
+        padding: '0% 10% 0% 10%'
+    },
+    ul: {
+        padding: '0% 15% 0% 15%'
     }
 }
